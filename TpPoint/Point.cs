a2f432ca5fdfff07a7200f29178590a9184e32d3
SylvainTorrenti <torrsyl@gmail.com>
2023-01-12 20:09:10 +0100

    Ajoutez des fichiers projet.

---
+++ TpPoint/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpPoint
+{
+    internal class Point
+    {
+        private int _abscissa;
+        private int _ordered;
+        public int Abscissa { get => _abscissa; set => _abscissa = value; }
+        public int Ordered { get => _ordered; set => _ordered = value; }
+        public Point()
+        {
+        }
+        private double Carre(double Coordonee)
+        {
+            return Coordonee * Coordonee;
+        }
+        public double Norme(Point point)
+        {
+            return Math.Sqrt(Carre(0 - Abscissa) + Carre(0 - Ordered));
+        }
+    }
+}
---
+++ TpPoint/Program.cs
@@ -0,0 +1,7 @@
+﻿using TpPoint;
+
+Point Point1 = new Point();
+Point1.Abscissa = 1;
+Point1.Ordered = 2;
+
+Console.WriteLine($"La norme du point (1,2) est : {Point1.Norme(Point1)}") ; 
